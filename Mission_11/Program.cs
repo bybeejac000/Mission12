53007a9110e051500054255476c9f7df5cce86a0
bybeejac000 <jake.bybee@bybeemail.com>
2025-03-30 17:54:50 -0600

    cors

--- Mission_11/Program.cs
+++ Mission_11/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3001")
+            policy.WithOrigins("https://lemon-wave-09349cf1e.6.azurestaticapps.net/")
             .AllowCredentials()
             .AllowAnyHeader()
             .AllowAnyMethod();
